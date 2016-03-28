3a1e538350e83fb476fdc8b08dba9b105f50b6b8
James Hartshorn <james@localhost.localdomain>
2016-03-28 23:08:13 +0100

    Fixed script (removed dot from dotfiles) and added mac files

--- commit_dotfiles.sh
+++ commit_dotfiles.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#git checkout mac
+git checkout mac
 #Copy dotfiles to ~/.dotfiles
-cp ~/.config/awesome/rc.lua ~/.dotfiles 
-cp ~/.xsessionrc ~/.dotfiles 
-cp ~/.zshrc ~/.dotfiles 
-cp ~/.xinitrc ~/.dotfiles 
-cp ~/.xbindkeysrc ~/.dotfiles 
-cp ~/.zlogin ~/.dotfiles 
-cp /etc/network/interfaces ~/.dotfiles
+cp ~/.config/awesome/rc.lua ~/dotfiles 
+cp ~/.xsessionrc ~/dotfiles 
+cp ~/.zshrc ~/dotfiles 
+cp ~/.xinitrc ~/dotfiles 
+cp ~/.xbindkeysrc ~/dotfiles 
+cp ~/.zlogin ~/dotfiles 
+cp /etc/network/interfaces ~/dotfiles
 
 #Obfuscate wpa key
-#sed -i '/wpa-psk/ c\\twpa-psk XXXXXXXX' ~/.dotfiles/interfaces
+sed -i '/wpa-psk/ c\\twpa-psk XXXXXXXX' ~/dotfiles/interfaces
 
 #Add, commit and push to github
-#git add -A  
-#git commit
-#git push origin mac 
+git add -A  
+git commit
+git push origin mac 
