33b1a257a0650c14a9b7d1ada360768a799100c7
James Hartshorn <james@localhost.localdomain>
2016-03-28 23:13:15 +0100

    Changed directory name (Removed dot)

--- commit_dotfiles.sh
+++ commit_dotfiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-#Copy dotfiles to ~/.dotfiles
-cp ~/.config/awesome/rc.lua ~/.dotfiles 
-cp ~/.xsessionrc ~/.dotfiles 
-cp ~/.zshrc ~/.dotfiles 
-cp ~/.xinitrc ~/.dotfiles 
-cp ~/.xbindkeysrc ~/.dotfiles 
-cp ~/.zlogin ~/.dotfiles 
-cp /etc/network/interfaces ~/.dotfiles
+git checkout master
+#Copy dotfiles to ~/dotfiles
+cp ~/.config/awesome/rc.lua ~/dotfiles 
+cp ~/.xsessionrc ~/dotfiles 
+cp ~/.zshrc ~/dotfiles 
+cp ~/.xinitrc ~/dotfiles 
+cp ~/.xbindkeysrc ~/dotfiles 
+cp ~/.zlogin ~/dotfiles 
+cp /etc/network/interfaces ~/dotfiles
 
 #Obfuscate wpa key
-sed -i '/wpa-psk/ c\\twpa-psk XXXXXXXX' ~/.dotfiles/interfaces
+sed -i '/wpa-psk/ c\\twpa-psk XXXXXXXX' ~/dotfiles/interfaces
 
 #Add, commit and push to github
 git add -A  
