bf877f11b9b84251035842b8ab0de7e644ed9cc6
James Hartshorn <james@localhost.localdomain>
2016-03-28 11:22:40 +0100

    Initial commit

---
+++ commit_dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Copy dotfiles to ~/.dotfiles
+cp ~/.config/awesome/rc.lua ~/.dotfiles 
+cp ~/.xsessionrc ~/.dotfiles 
+cp ~/.zshrc ~/.dotfiles 
+cp ~/.xinitrc ~/.dotfiles 
+cp ~/.xbindkeysrc ~/.dotfiles 
+cp ~/.zlogin ~/.dotfiles 
+cp /etc/network/interfaces ~/.dotfiles
