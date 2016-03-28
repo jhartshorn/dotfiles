4da26317f135784b34a4e7e9dde02b3abf510e5c
James Hartshorn <james@localhost.localdomain>
2016-03-28 12:38:02 +0100

    Added git commands to commit_dotfiles.sh

--- commit_dotfiles.sh
+++ commit_dotfiles.sh
@@ -12,3 +12,8 @@ cp /etc/network/interfaces ~/.dotfiles
 #Obfuscate wpa key
 sed -i '/wpa-psk/ c\\twpa-psk XXXXXXXX' ~/.dotfiles/interfaces
 
+#Add, commit and push to github
+git add .*  
+git add *  
+git commit
+git push origin master
