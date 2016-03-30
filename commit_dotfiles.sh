8643eeb7b00b73026ad8ba6e429b92acccd87fb2
James Hartshorn <james@localhost.localdomain>
2016-03-30 19:24:50 +0100

    Added new config files

--- commit_dotfiles.sh
+++ commit_dotfiles.sh
@@ -2,11 +2,17 @@
 git checkout mac
 #Copy dotfiles to ~/.dotfiles
 cp ~/.config/awesome/rc.lua ~/dotfiles 
+cp /usr/share/awesome/themes/default/theme.lua ~/dotfiles
 cp ~/.xsessionrc ~/dotfiles 
 cp ~/.zshrc ~/dotfiles 
 cp ~/.xinitrc ~/dotfiles 
 cp ~/.xbindkeysrc ~/dotfiles 
 cp ~/.zlogin ~/dotfiles 
+cp /usr/share/X11/xorg.conf.d/50-macbookkb.conf ~/dotfiles
+cp /etc/nsswitch.conf ~/dotfiles
+cp /etc/default/crda ~/dotfiles
+
+#Network Interfaces
 cp /etc/network/interfaces ~/dotfiles
 cp /etc/network/interfaces.AppleNetwork ~/dotfiles
 cp /etc/network/interfaces.Apple5Ghz ~/dotfiles
